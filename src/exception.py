f4e856f81429568196d1c114201356080a8f07cb
Shahper Shahin <74124339+ShahperShahin@users.noreply.github.com>
2023-06-12 13:45:46 +0530

    Update exception.py

--- src/exception.py
+++ src/exception.py
@@ -20,11 +20,4 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divided by zero error")
-        raise CustomException(e,sys)
+  
